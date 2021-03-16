1337493d33a0fed766e4d262ec6482f154221b97
Colin Tseng <klt.aus@yahoo.com>
2021-03-16 22:32:41 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 12;
+let num4 = 3;
+let obj = [0,3,4,5,20];
+
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 10) + 1;;
+console.log(random);
+
+let mod = num3 / num4;
+
+let max = Math.max(1,3,20);
